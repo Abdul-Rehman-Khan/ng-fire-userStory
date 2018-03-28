a27ae519fdb92b7196a193d4b9ec2c8a7ae769ac
Sennen Desouza <sennendesouza91@gmail.com>
2018-03-27 18:11:23 -0700

    data binding to a user story

--- userStory/src/app/app.component.ts
+++ userStory/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+interface userStory {
+  user: string,
+  story: string
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  userStory: userStory =
+    {
+      'user': '',
+      'story': ''
+    }
+
 }
